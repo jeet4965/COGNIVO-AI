6b2c7473703e0c9b6070ee3979aa1ef1e14be3c6
Jeet Mahata <jeetmahata57@gmail.com>
2026-09-09 01:31:41 +0530

    fix baseURL to point to deployed backend

--- src/features/auth/services/auth.api.js
+++ src/features/auth/services/auth.api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: "https://cognivo-backend-p704.onrender.com",
     withCredentials: true
 })
 export async function register({username, email, password}){
